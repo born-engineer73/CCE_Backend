85b94a960556dba7aef189a7d18792391658cc5b
Vaibhav <vaibhav.kesharwani07march@gmail.com>
2024-11-07 13:42:42 +0530

    final commit

--- app.py
+++ app.py
@@ -36,5 +36,9 @@ def add_news():
 
     return redirect(url_for('index'))
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Use Railway's port if available
+    app.run(host="0.0.0.0", port=port)
